9ce4cef90475f856f03b980731a6c2895f21b506
yang-chris <18813068824@163.com>
2019-03-27 23:09:26 +0800

    init

--- api_dev.js
+++ api_dev.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
+const bodyParser = require('body-parser');
 
 app.use(cors({
   origin:['http://localhost:8080'],
   methods:['GET','POST'],
   alloweHeaders:['Content-Type', 'Authorization']
 }));
- 
+ // need it...  
+app.use(bodyParser.urlencoded({ extended: false }));  
+
+
 app.get('/', function(req, res){
   res.send({a:'hello world'});
 });
 // 第一个nodeJs接口，接收
 app.get('/test_api', function(req, res){
+  console.log('开始')
   res.send({a:123});
+});
+
+let JSON = null
+// 接收参数接收
+app.post('/sendData', function(req, res){
+  console.log(JSON,'****');
+  res.send({code: 0, msg: 'ok'});
+});
+app.get('/sendData', function(req, res){
+  console.log(JSON,'****');
+  res.send({code: 0, msg: 'ok'});
 });
 
 app.listen( 8081,function(){
