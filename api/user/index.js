64b058265db527418abe7d47d953b6e1057fdde9
yangxiaowei <18813068824@163.com>
2019-09-24 00:02:50 +0800

    登录

--- api/user/index.js
+++ api/user/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const { successSend, failSend} = require('../../utils/utils.js')
+const userPath = `${__dirname}/user.json`;
 router.post('/register', bodyParser.json(),function(req, res){
   const param = req.body;
   const userParam = {
@@ -11,7 +12,6 @@ router.post('/register', bodyParser.json(),function(req, res){
     creatTime: Date.now()
   }
   Object.assign(userParam, param);
-  const userPath = `${__dirname}/user.json`;
   // 判断文件是否存在
   fs.exists(userPath, (exists) => {
     if(exists){
@@ -21,6 +21,12 @@ router.post('/register', bodyParser.json(),function(req, res){
           return err
         }
         const userList = JSON.parse(data);
+        for(let item of userList){
+          if(item.username === userParam.username){
+            res.send(failSend('用户名已存在，请更改用户名！'))
+            return;
+          }
+        }
         userList.push(userParam);
         writeFile(res, JSON.stringify(userList))
       })
@@ -31,12 +37,45 @@ router.post('/register', bodyParser.json(),function(req, res){
   })
 })
 
+router.post('/login', bodyParser.json(), (req, res) => {
+  const param = req.body;
+  console.log(param,'**param')
+  const {username, password} = param;
+  readFile(userPath).then(data => {
+    const userList = JSON.parse(data);
+    for(let item of userList){
+      console.log(item['username'], '**', username)
+      if(item.username === username){
+        if(item.password !== password){
+          res.send(failSend('用户名或密码错误'));
+          return;
+        }
+        res.send(successSend('成功'));
+        return;
+      }
+    }
+    res.send(failSend('当前用户名不存在，请先注册'))    
+  })
+  
+})
 function writeFile(res, data){
   fs.writeFile(`${__dirname}/user.json`, data, err => {
     if(err){
       return err
     }
     res.send(successSend('注册成功'));
+  })
+}
+
+function readFile(data){
+  return new Promise( (reslove, reject) => {
+    fs.readFile(data, 'utf-8', (err, data) => {
+      if(err){
+        reject(new Error('读取文件失败'));
+        return;
+      }
+      reslove(data)
+    })
   })
 }
 
